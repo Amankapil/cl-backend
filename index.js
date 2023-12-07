57a4903edf53cf6be5761b3a544b18393a018837
Aman <developer@codelinear.com>
2023-12-07 16:26:16 +0530

    codelinear-backend

---
+++ index.js
@@ -0,0 +1,44 @@
+import express from "express";
+import cors from "cors";
+import mysql from "mysql2";
+import bodyParser from "body-parser";
+import multer from "multer";
+// import cookieParser from "cookie-parser";
+// import bcrypt from "bcrypt";
+import webroute from "./routes/routes.js";
+
+const app = express();
+
+app.use(express.json());
+// app.use(express.urlencoded());
+app.use(cors());
+app.use(bodyParser.json());
+// app.use(cookieParser());
+
+app.use("/code", webroute);
+
+// const db = mysql.createConnection({
+//   host: "127.0.0.1",
+//   user: "syedsaif",
+//   password: "5LIxqktlabYiGcdF",
+//   database: "dashboard",
+// });
+const db = mysql.createPool({
+  host: "217.21.87.205",
+  user: "u947451844_saif08",
+  password: "u]1ro&X$1R",
+  database: "u947451844_pages",
+});
+db.getConnection((err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("Database Connected");
+  }
+});
+
+const port = process.env.PORT || 4500;
+app.listen(port, () => {
+  console.log(`app running on port ${port}...`);
+});
+export default db;
